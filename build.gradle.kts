3b604f16e9a79b22326b7c84bb5c9b8bfa3041ac
waxew <4622.ar@gmail.com>
2026-08-30 03:56:03 +0330

    fix: align KSP with shared Core

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "9.2.1" apply false
     id("com.android.library") version "9.2.1" apply false
-    id("com.google.devtools.ksp") version "2.3.10" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
