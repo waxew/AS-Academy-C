331618a9490355b0ba7102ea18234a9d507c96e1
waxew <4622.ar@gmail.com>
2026-08-30 04:07:27 +0330

    fix: align Kotlin plugins with shared Core

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application") version "9.2.1" apply false
     id("com.android.library") version "9.2.1" apply false
-    id("com.google.devtools.ksp") version "2.3.11" apply false
+    id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.10" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
 }
